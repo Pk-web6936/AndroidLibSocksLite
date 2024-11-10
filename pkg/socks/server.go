790df6f32d05fd9c5821f26499c8c9895ff2d578
DecorativeFamily <185765765+decorativefamily@users.noreply.github.com>
2024-11-10 20:05:27 +0330

    Update server.go

--- pkg/socks/server.go
+++ pkg/socks/server.go
@@ -14,6 +14,12 @@ import (
 
 const CoreVersion = "1.0.0"
 
+package socks
+
+func CheckCoreVersion() string {
+return CoreVersion 
+}
+
 type User struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
