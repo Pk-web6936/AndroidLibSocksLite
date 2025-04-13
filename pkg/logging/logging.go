c43170d6550a9d199ed858ad324eac04a2de17f1
Pk-web6936 <202365630+Pk-web6936@users.noreply.github.com>
2025-04-13 21:10:52 +0330

    Update logging.go

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -1,18 +1,29 @@
 package logging
 
-import "log"
+import (
+	"github.com/sirupsen/logrus"
+)
 
 // LogInfo logs informational messages with an [INFO] prefix.
 func LogInfo(message string) {
-	logMessage("[INFO]", message)
+	logMessage(logrus.InfoLevel, message)
 }
 
 // LogError logs error messages with an [ERROR] prefix.
 func LogError(message string) {
-	logMessage("[ERROR]", message)
+	logMessage(logrus.ErrorLevel, message)
 }
 
 // logMessage is a helper function to format and log messages.
-func logMessage(level, message string) {
-	log.Printf("%s %s\n", level, message)
+func logMessage(level logrus.Level, message string) {
+	logrus.WithFields(logrus.Fields{
+		"level": level.String(),
+	}).Log(level, message)
+}
+
+func init() {
+	// Set log format to JSON for better structure and parsing
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	// Set log level to info as default
+	logrus.SetLevel(logrus.InfoLevel)
 }
